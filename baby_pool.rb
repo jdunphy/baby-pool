9d5ae9ad9c16e3d5e01eefde0cb685fbab1c3d88
Jacob Dunphy <jacob.dunphy@gmail.com>
2009-06-12 19:11:28 -0700

    Removing the entry form. Voting is closed!

--- baby_pool.rb
+++ baby_pool.rb
@@ -24,44 +24,5 @@ class BabyPool < Sinatra::Base
     @guesses = Guess.order(:created_at.desc).all
     haml :index
   end
-
-  post '/guesses' do
-    guess = Guess.new(params['guess'])
-    guess.birth_date= "#{params['month']}/#{params['day']}/2009"
-    guess.created_at = Time.now
-    if guess.valid?
-      guess.save
-      redirect '/'
-    else
-      @guesses = Guess.all
-      @guess = guess
-      haml :index
-    end
-  end
-
-  helpers do
-
-    def selected(current, selection)
-      current == selection ? "selected" : ""
-    end
-    
-    def month_options(selection = nil)
-      out = ""
-      {6 => "Jun", 7 => "Jul", 8 => "Aug" }.
-        each do |num, month|
-        out << "<option value='#{num}' #{selected(num, selection)}>#{month}</option>\n"
-      end
-      out
-    end
-
-    def day_options(selection = nil)
-      out = ""
-      (1..31).each do |day|
-        out << "<option #{selected(day,selection)}>#{day}</option>\n"
-      end
-      out
-    end
-
-  end
   
 end
